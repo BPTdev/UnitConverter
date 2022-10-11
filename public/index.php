62cb34aeb1d1ed4a448efa00bf3e496056414bc9
Benoit <benoit.pierrehumbert.pro@gmail.com>
2022-10-11 13:37:25 +0200

    initial

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Acceuil</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/dist/output.css" rel="stylesheet">
+    <script src="../path/to/flowbite/dist/flowbite.js"></script>
+</head>
+<body>
+    <div class="container bg-purple-900">
+        <div class="header">
+            <h1>Accueil</h1>
+        </div>
+    </div>
+</body>
+</html>
