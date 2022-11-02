5cfcf612f2d7770088686b2e7e96d8e1d800e02e
Benoit <benoit.pierrehumbert.pro@gmail.com>
2022-11-02 15:21:51 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,13 +9,36 @@
     <link href="/css/fonts.css" rel="stylesheet">
 </head>
 <body>
-<div class="text-7xl font-family ">
-    <div class="flow-root">
+<div class="text-7xl font-family flex-wrap">
+    <div class="flow-root title">
         <p class="logo-purple float-left ml-10">1</p>
         <p class="logo-purple float-right mr-10">2</p>
     </div>
+    <div class="flow-root name text-2xl mt-10">
+        <div class="float-left ml-5"><p>BONJOUR</p></div>
+        <div class="float-right mr-5"><p>YOURNAME</p></div>
+    </div>
+    <div class="mx-5 separator"><hr class="text-white"></div>
+    <div class="flow-root convert text-white text-2xl mt-2">
+        <div class="float-left ml-5">UNITÉ</div>
+        <div class="absolute right-2/4 translate-x-1/2"><=></div>
+        <div class="float-right mr-5">UNITÉ</div>
+    </div>
+    <div class="flow-root convert text-white text-2xl mt-2">
+        <form action="#" >
+            <div class="flow-root">
+            <div class="float-left ml-5 w-2/5"> <input type="text" class="w-full rounded bg-[#933ea4] border- 4border-[#490456]"></div>
+            <div class="float-right mr-5 w-2/5"><input type="text" class="w-full"></div>
+        </div>
+        </form>
+    </div>
+    <div class="flow-root title">
+        <div class="float-left ml-5"></div>
+        <div class="float-right mr-5"></div>
+    </div>
     <div class="content">
 
+    </div>
 </div>
 </body>
 </html>
